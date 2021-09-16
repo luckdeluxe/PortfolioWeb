485782c516f1cd161ee3bbd6d668f03de31a594a
Lucas Sanchez <luckdeveloper@hotmail.com>
2021-09-16 18:48:04 -0300

    Upgrade language from Spanish to English

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,18 +1,17 @@
 from django.db import models
 
-# Create your models here.
 class Project(models.Model):
-    title = models.CharField(max_length=200, verbose_name="Título")
-    description = models.TextField(verbose_name="Descripción")
-    image = models.ImageField(verbose_name="Imagen", upload_to="projects")
-    link = models.URLField(verbose_name="Dirección Web", null=True, blank=True)
-    cfuente = models.URLField(verbose_name="Codigo fuente", null=True, blank=True)
-    created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
-    updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de edición")
+    title = models.CharField(max_length=200, verbose_name="Title")
+    description = models.TextField(verbose_name="Description")
+    image = models.ImageField(verbose_name="Image", upload_to="projects")
+    link = models.URLField(verbose_name="Web address", null=True, blank=True)
+    cfuente = models.URLField(verbose_name="Source code", null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Creation date")
+    updated = models.DateTimeField(auto_now=True, verbose_name="Edition date")
 
     class Meta:
-        verbose_name = "proyecto"
-        verbose_name_plural = "proyectos"
+        verbose_name = "Project"
+        verbose_name_plural = "Projects"
         ordering = ["-created"]
 
     def __str__(self):
